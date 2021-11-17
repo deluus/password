dd67863ec09b52dfb4195d3a9e72052c8910459a
deluus polycarp <pdeluus@gmail.com>
2021-11-17 10:00:34 -0800

    hw3

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var numbers =["1","2","3","4","5","6","7","8","9","0"];
 
 
 // what does my app need from the user?
+
 // criteria : a password length 
            // b character types 
            // c confirm whether to include lowerCase, uppercase, numeric, and/or special characters
@@ -25,15 +26,41 @@ var isNumbers = true;
 var isSpecials = true;
 
 
+function generatePassword (){
+  var password ="";
+  var characters = [];
+  
 
+//  alert for password length
 console.log (alert("Your password must be between 8 and 128 characters"));
 
-console.log (prompt("How many characters do you want in your password?"));
+//  ask for desired length of password
+var result =window.prompt ("How many characters do you want in your password");
 
-if( passwordLength < 8 && paswordLength >128){
-  console.log ( alert("Your password must be between 8 and 128 characters"));
+// stores results
+console.log (result);
 
+// statements to validate criterias are met
+// if nothing is entered, user will need to start over
+if (result == ""){
+alert ("you must enter a number")
+return "";
 }
+
+// if less than 8 is entered, user will start over
+else if(result < 8){
+  alert ("you entered a number less than 8")
+  return ""; 
+}
+
+// if more than 128 is entered, user will start over
+else if (result >128){
+  alert ("you entered a number more thanh 128")
+  return "";
+}
+
+
+//  user to agree that following characters will be in the password
 
 console.log (confirm("Must have Uppercase letters"));
 console.log (confirm("Must have Lowercase letters"));
@@ -41,37 +68,29 @@ console.log (confirm("Must have Numbers"));
 console.log (confirm("Must have special characters"));
 
 
-// validate input
-if (isMin)
+
 
 
 
 // creating radomn selectors
 
-// let randomValue = upperCase [Math.floor(Math.random()* upperCase.lenth)];
-// let randomValue = lowerCase [Math.floor(Math.random()* lowerCase.lenth)];
-// let randomValue = specialCharacters [Math.floor(Math.random()* specialCharacters.lenth)];
-// let randomValue = numbers [Math.floor(Math.random()* numbers.lenth)];
+let randomUpper = upperCase [Math.floor(Math.random()* upperCase.lenth)];
+let randomLower = lowerCase [Math.floor(Math.random()* lowerCase.lenth)];
+let randomSpecial = specials [Math.floor(Math.random()* specials.lenth)];
+let randomNumber = numbers [Math.floor(Math.random()* numbers.lenth)];
 
 
 // generate password
 // display the password
 
 
+}
 
-function generatePassword (){
-  var password ="";
-  var characters = [];
+
+console.log ('generate a password'); {
   
-
-
-
-console.log ('generate a password')
-
-
-
-return password;
-
+  
+  // return password;
 }
 
 function writePassword() {
@@ -84,3 +103,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
